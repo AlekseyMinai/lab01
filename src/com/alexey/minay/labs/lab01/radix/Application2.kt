ff6f0257a0d73e29dfc00f25d7fecdb86404e48e
alexey.minai <aleksejzuka@gmail.com>
2020-03-22 22:05:17 +0300

    [lab02 t2]  deleted service and di levels

--- src/com/alexey/minay/labs/lab01/radix/Application2.kt
+++ src/com/alexey/minay/labs/lab01/radix/Application2.kt
@@ -1,24 +1,45 @@
 package com.alexey.minay.labs.lab01.radix
 
-import com.alexey.minay.labs.lab01.radix.di.MainComponent
+import com.alexey.minay.labs.lab01.radix.radix.RadixChangerImpl
+import com.alexey.minay.labs.lab01.radix.radix.RadixChangerResult
+import com.alexey.minay.labs.lab01.radix.validator.ArgsValidatorImpl
 import com.alexey.minay.labs.lab01.radix.validator.Messages
+import com.alexey.minay.labs.lab01.radix.view.ConsoleView
 
 class Application2 {
 
     companion object {
         @JvmStatic
         fun main(args: Array<String>) {
-            val radixService = MainComponent.getRadixService()
             if (args.size != 3) {
-                radixService.error(Messages.INCORRECT_ARGS_QUANTITY)
+                print(Messages.INCORRECT_ARGS_QUANTITY)
                 return
             }
-            radixService.change(
+            Application2().change(
                     oldRadix = args[0],
                     newRadix = args[1],
                     value = args[2]
             )
         }
+    }
+
+    private val view = ConsoleView()
+    private val validator = ArgsValidatorImpl()
+    private val numbers: List<Char> = listOf('0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E',
+            'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z')
+    private val radixChanger = RadixChangerImpl(validator, numbers)
+
+    fun change(oldRadix: String, newRadix: String, value: String) {
+        when (val result = radixChanger.change(oldRadix, newRadix, value)) {
+            is RadixChangerResult.Success -> view.showResult(result.value)
+            is RadixChangerResult.IncorrectNewRadix -> view.showResult(Messages.INCORRECT_NEW_RADIX)
+            is RadixChangerResult.IncorrectOldRadix -> view.showResult(Messages.INCORRECT_OLD_RADIX)
+            is RadixChangerResult.IncorrectValue -> view.showResult(Messages.INCORRECT_VALUE)
+        }
+    }
+
+    fun error(message: String) {
+        view.showResult(message)
     }
 }
 
--- src/com/alexey/minay/labs/lab01/radix/service/ChangeRadixService.kt
+++
@@ -1,6 +0,0 @@
-package com.alexey.minay.labs.lab01.radix.service
-
-interface ChangeRadixService {
-    fun change(oldRadix: String, newRadix: String, value: String)
-    fun error(message: String)
-}
