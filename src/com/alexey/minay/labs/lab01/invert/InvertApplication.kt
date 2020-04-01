b5df7e26b233616422ffc0f0acc4912300340005
alexey.minai <aleksejzuka@gmail.com>
2020-04-01 22:33:05 +0300

    [lab01] deleted not necessary code

--- src/com/alexey/minay/labs/lab01/invert/InvertApplication.kt
+++ src/com/alexey/minay/labs/lab01/invert/InvertApplication.kt
@@ -6,29 +6,24 @@ import com.alexey.minay.labs.lab01.invert.matrix.ReaderState
 import com.alexey.minay.labs.lab01.invert.matrix.handler.DataHendlerImpl
 import com.alexey.minay.labs.lab01.invert.view.ConsoleView
 
-class InvertApplication {
 
-    companion object {
-        @JvmStatic
-        fun main(args: Array<String>) {
-            if (args.isNullOrEmpty()) {
-                print("Отсутствует аргумент")
-                return
-            }
-            InvertApplication().invert(args[0])
-        }
+fun main(args: Array<String>) {
+    if (args.isNullOrEmpty()) {
+        print("Отсутствует аргумент")
+        return
     }
+    invert(args[0])
+}
 
-    private fun invert(inputData: String) {
-        val view = ConsoleView()
-        val dataHandler = DataHendlerImpl()
-        val matrixReader = FileMatrixReader(dataHandler, inputData)
-        when (val readerState = matrixReader.read()) {
-            is ReaderState.Error -> view.showError("error..")
-            is ReaderState.Success -> {
-                val invertMatrix = MatrixUtils.invert(readerState.matrix)
-                view.showResult(invertMatrix)
-            }
+private fun invert(inputData: String) {
+    val view = ConsoleView()
+    val dataHandler = DataHendlerImpl()
+    val matrixReader = FileMatrixReader(dataHandler, inputData)
+    when (val readerState = matrixReader.read()) {
+        is ReaderState.Error -> view.showError("error..")
+        is ReaderState.Success -> {
+            val invertMatrix = MatrixUtils.invert(readerState.matrix)
+            view.showResult(invertMatrix)
         }
     }
 }
--- src/com/alexey/minay/labs/lab01/radix/RadixApplication.kt
+++ src/com/alexey/minay/labs/lab01/radix/RadixApplication.kt
@@ -6,36 +6,31 @@ import com.alexey.minay.labs.lab01.radix.validator.ArgsValidatorImpl
 import com.alexey.minay.labs.lab01.radix.validator.Messages
 import com.alexey.minay.labs.lab01.radix.view.ConsoleView
 
-class RadixApplication {
-
-    companion object {
-        @JvmStatic
-        fun main(args: Array<String>) {
-            if (args.size != 3) {
-                print(Messages.INCORRECT_ARGS_QUANTITY)
-                return
-            }
-            RadixApplication().change(
-                    oldRadix = args[0],
-                    newRadix = args[1],
-                    value = args[2]
-            )
-        }
+fun main(args: Array<String>) {
+    if (args.size != 3) {
+        print(Messages.INCORRECT_ARGS_QUANTITY)
+        return
     }
+    change(
+            oldRadix = args[0],
+            newRadix = args[1],
+            value = args[2]
+    )
+}
 
-    fun change(oldRadix: String, newRadix: String, value: String) {
-        val view = ConsoleView()
-        val validator = ArgsValidatorImpl()
-        val numbers: List<Char> = listOf('0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E',
-        'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z')
-        val radixChanger = RadixChangerImpl(validator, numbers)
-        when (val result = radixChanger.change(oldRadix, newRadix, value)) {
-            is RadixChangerResult.Success -> view.showResult(result.value)
-            is RadixChangerResult.IncorrectNewRadix -> view.showResult(Messages.INCORRECT_NEW_RADIX)
-            is RadixChangerResult.IncorrectOldRadix -> view.showResult(Messages.INCORRECT_OLD_RADIX)
-            is RadixChangerResult.IncorrectValue -> view.showResult(Messages.INCORRECT_VALUE)
-            is RadixChangerResult.TooBigValue -> view.showResult(Messages.INCORRECT_VALUE)
-        }
+fun change(oldRadix: String, newRadix: String, value: String) {
+    val view = ConsoleView()
+    val validator = ArgsValidatorImpl()
+    val numbers: List<Char> = listOf('0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E',
+            'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z')
+    val radixChanger = RadixChangerImpl(validator, numbers)
+    when (val result = radixChanger.change(oldRadix, newRadix, value)) {
+        is RadixChangerResult.Success -> view.showResult(result.value)
+        is RadixChangerResult.IncorrectNewRadix -> view.showResult(Messages.INCORRECT_NEW_RADIX)
+        is RadixChangerResult.IncorrectOldRadix -> view.showResult(Messages.INCORRECT_OLD_RADIX)
+        is RadixChangerResult.IncorrectValue -> view.showResult(Messages.INCORRECT_VALUE)
+        is RadixChangerResult.TooBigValue -> view.showResult(Messages.INCORRECT_VALUE)
     }
 }
+
 
