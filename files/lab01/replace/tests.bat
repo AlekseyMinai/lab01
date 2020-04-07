24c803cd1ca6b2c05db84a71693f4c9b9d006a34
alexey.minai <aleksejzuka@gmail.com>
2020-04-07 22:02:32 +0300

    [lab 01] added replace.jar

--- files/lab01/replace/tests.bat
+++ files/lab01/replace/tests.bat
@@ -1,4 +1,4 @@
-java -jar Lab01.jar text1.txt result1.txt Skibidi 345345
+java -jar replace.jar text1.txt result1.txt Skibidi 345345
 иди
-java -jar Lab01.jar text2.txt result2.txt 1231234 !!!
-java -jar Lab01.jar text3.txt result3.txt 123 !!!
+java -jar replace.jar text2.txt result2.txt 1231234 !!!
+java -jar replace.jar text3.txt result3.txt 123 !!!
