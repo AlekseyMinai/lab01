1f1e3452fdedb71cc534db5c00fecc7adcbcaf1b
alexey.minai <aleksejzuka@gmail.com>
2020-04-10 00:28:44 +0300

    [lab02 t6] added reading and writing files

--- src/com/alexey/minay/labs/lab02/template06/Application.kt
+++ src/com/alexey/minay/labs/lab02/template06/Application.kt
@@ -1,9 +1,43 @@
 package com.alexey.minay.labs.lab02.template06
 
-import org.junit.Assert
+import java.io.File
+import java.nio.charset.Charset
 
 fun main(args: Array<String>) {
+    if (args.size < 2 || args.size % 2 != 0) {
+        println("Incorrect params")
+        return
+    }
+    val file = File(args[0])
+    if (!file.exists()) {
+        println("File doesn't exist")
+        return
+    }
 
+    val template = readTextFrom(file)
+    val params = getParamsFrom(args)
+    val resultText = expandTemplate(template, params)
+    write(resultText, args[1])
+}
+
+private fun readTextFrom(file: File): String {
+    val bufferedReader = file.bufferedReader()
+    val iterator = bufferedReader.lineSequence().iterator()
+    val template = StringBuilder()
+    iterator.forEach {
+        template.append(it)
+        template.append("\n")
+    }
+    bufferedReader.close()
+    return template.toString()
+}
+
+private fun getParamsFrom(args: Array<String>): Map<String, String> {
+    val params = mutableMapOf<String, String>()
+    for (i in 2 until args.size step 2) {
+        params[args[i]] = args[i + 1]
+    }
+    return params
 }
 
 fun expandTemplate(template: String, params: Map<String, String>): String {
@@ -53,4 +87,11 @@ private fun replace(replacedTemplate: String, result: Result, params: Map<String
 private fun calculateOffset(offset: Int, params: Map<String, String>, param: String): Int {
     val value = params[param] ?: return 0
     return offset + value.length - param.length
+}
+
+private fun write(text: String, fileName: String) {
+    val file = File(fileName)
+    val bufferedWriter = file.bufferedWriter(Charset.forName("Windows-1251"))
+    bufferedWriter.write(text)
+    bufferedWriter.close()
 }
